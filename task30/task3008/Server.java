c75ff374c0f86ec76c9e6a21edca529ae64d10a6
lenaz <lena.zelenaya@gmail.com>
2020-04-15 16:39:52 +0300

    Made Serversocket instance and Handler

--- task30/task3008/Server.java
+++ task30/task3008/Server.java
@@ -1,7 +1,26 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            System.out.println("Server has started");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+    private static class Handler extends Thread {
+        private Socket socket;
 
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run(){
+        }
     }
 }
