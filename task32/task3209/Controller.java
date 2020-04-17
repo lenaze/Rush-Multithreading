3d73f912436808ed87cd44e775a1484a5c7e382d
lenaz <lena.zelenaya@gmail.com>
2020-04-17 21:09:57 +0300

    HTML Redactor. Swing libraries, MVC, Listeners

---
+++ task32/task3209/Controller.java
@@ -0,0 +1,159 @@
+package com.javarush.task.task32.task3209;
+
+import javafx.scene.web.HTMLEditor;
+
+import javax.swing.*;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import java.io.*;
+
+public class Controller {
+    private View view;
+    private HTMLDocument document = new HTMLDocument();
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public HTMLDocument getDocument() {
+        return document;
+    }
+
+    public void init() {
+        createNewDocument();
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+
+        document.addUndoableEditListener(view.getUndoListener());
+
+        view.update();
+    }
+
+    public void setPlainText(String text) {
+        if (document != null) resetDocument();
+        StringReader reader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(reader, document, 0);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+        String htmlString = writer.toString();
+        return htmlString;
+    }
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.init();
+        controller.init();
+        view.setController(controller);
+
+    }
+
+
+    public void createNewDocument() {
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML редактор");
+        view.resetUndo();
+        currentFile = null;
+
+    }
+
+    public void openDocument() {
+        // Метод должен работать аналогично методу saveDocumentAs(), в той части, которая отвечает за выбор файла
+
+        //Переключать представление на html вкладку
+        view.selectHtmlTab();
+        //Создавать новый объект для выбора файла JFileChooser
+        JFileChooser jFileChooser = new JFileChooser();
+        //Устанавливать ему в качестве фильтра объект HTMLFileFilter
+        jFileChooser.setFileFilter(new HTMLFileFilter());
+        //Показывать диалоговое окно "Save File" для выбора файла
+        int n = jFileChooser.showOpenDialog(view);
+
+        //Когда файл выбран, необходимо
+        if (n == JFileChooser.APPROVE_OPTION) {
+            //Установить новое значение currentFile
+            currentFile = jFileChooser.getSelectedFile();
+            //Сбросить документ
+            resetDocument();
+            //Установить имя файла в заголовок у представления
+            view.setTitle(currentFile.getName());
+
+            //Создать FileReader, используя currentFile
+            try (FileReader fileReader = new FileReader(currentFile)) {
+                //Вычитать данные из FileReader-а в документ document с помощью объекта класса
+                new HTMLEditorKit().read(fileReader, document, 0);
+                //Сбросить правки
+                view.resetUndo();
+            }
+            catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
+    }
+
+    public void saveDocument() {
+        if (currentFile == null) {
+            saveDocumentAs();
+        } else {
+            //Переключать представление на html вкладку
+            view.selectHtmlTab();
+
+            //Создавать FileWriter на базе currentFile
+            try (FileWriter fileWriter = new FileWriter(currentFile)) {
+                //Переписывать данные из документа document в объекта FileWriter-а аналогично тому, как мы это делали в методе getPlainText()
+                new HTMLEditorKit().write(fileWriter, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
+    }
+
+    public void saveDocumentAs() {
+        view.selectHtmlTab();
+        JFileChooser jFileChooser = new JFileChooser();
+
+        jFileChooser.setFileFilter(new HTMLFileFilter());
+
+        int n = jFileChooser.showSaveDialog(view);
+
+
+        if (n == JFileChooser.APPROVE_OPTION) {
+
+            currentFile = jFileChooser.getSelectedFile();
+
+            view.setTitle(currentFile.getName());
+
+            try (FileWriter fileWriter = new FileWriter(currentFile)) {
+                
+                new HTMLEditorKit().write(fileWriter, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
+
+    }
+}
